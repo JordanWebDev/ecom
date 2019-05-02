a6d332ce90686ca72ca9cd1a8831fdf400f1ea15
JordanWebDev <JordanWebDev@users.noreply.github.com>
2019-05-02 09:30:35 -0600

    composer & npm update, also restored view files

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,12 +9,6 @@ require('./bootstrap');
 require('../../../node_modules/jquery/dist/jquery.min')
 require('../../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.min')
 
-require('./move-top')
-require('./easing')
-require('../../../node_modules/flexslider/jquery.flexslider-min')
-require('./minicart.min')
-require('./site')
-
 window.Vue = require('vue');
 
 /**
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,22 @@
+<footer>
+    <div class="row">
+        <div class="col-md-4">
+            &copy; Website name | 2019
+        </div>
+        <div class="col-md-4">
+            Menu
+        </div>
+        <div class="col-md-4">
+            About us
+        </div>
+        <div class="col-md-4">
+            Contact us
+        </div>
+        <div class="col-md-4">
+            Privacy Policy
+        </div>
+        <div class="col-md-4">
+            Terms of Service
+        </div>
+    </div>
+</footer>
