f7d40c74a418fc1d75120efb51ef5abe453c2ec7
JordanWebDev <JordanWebDev@users.noreply.github.com>
2019-05-04 06:08:10 -0600

    added images and videos also created checkout and success blades

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,31 +16,14 @@
         @include('partials.nav')
         <main>
             <section>
-                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div>
 
-                    <div class="carousel-inner">
+                    <img src="../images/iceyourbeergif.gif" alt="beer-gif" height="100%" width="100%"/>
 
-                        <div class="carousel-item active">
-                            <img class="d-block w-100" src="../images/winepromo.png" alt="First slide" height="600px">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="../images/iceyourbeer.png" alt="Second slide" height="600px">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="../images/liquorjpg.jpg" alt="Third slide">
-                        </div>
-                    </div>
-                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
                 </div>
             </section>
             <section>
+                
             </section>
         </main>
         <footer>
